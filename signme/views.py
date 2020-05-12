4f3dbb8c1a5a76791e56235f3a678afe3b57d7f1
ashish1sasmal <ashishsasmal1@gmail.com>
2020-05-12 13:13:01 +0530

    first commit

---
+++ signme/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from base64 import b64decode
+from .models import Signuser
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
+import base64
+
+# Create your views here.
+def home(request):
+    if request.method == "POST":
+
+        image_b64 = request.POST.get('canva') # This is your base64 string image
+        format, imgstr = image_b64.split(';base64,')
+        ext = format.split('/')[-1]
+        data = ContentFile(base64.b64decode(imgstr), name=request.POST.get('username')+'.' + ext)
+        # print(data)
+        # data_uri = data
+        #
+        # header, encoded = data_uri.split(",", 1)
+        # data = b64decode(encoded)
+        # print(request.FILES.get('file'))
+        d = Signuser(username=request.POST.get('username'),stamp=data)
+        d.save()
+        # with open("media/"+request.POST.get('username')+".jpg", "wb") as f:
+        #     f.write(data)
+        print('image save')
+        return HttpResponse('Signature Save Successfully!')
+    else:
+        return render(request,'signme/index.html')
