1b99dfef5edec840c6fc1e164fd5251734a8df36
ashish1sasmal <ashishsasmal1@gmail.com>
2020-05-19 01:54:30 +0530

    asdas

--- signme/models.py
+++ signme/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Signuser(models.Model):
+    username = models.CharField(max_length=50)
+    stamp = models.ImageField(upload_to="signatures")
